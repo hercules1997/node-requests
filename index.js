a62b93a62aca2a00cb27aa02d9e4a247765d406c
Hércules C Andrade <hercules.chaves.andrade@gmail.com>
2023-01-26 15:25:54 +0000

    Configurando para deploy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const { request, response, json } = require('express')
 const express = require('express')
 const uuid = require('uuid')
-const port = 3006
+const port = process.env.PORT || 3001;
 const app = express()
 app.use(express.json())
 app.use(cors())
